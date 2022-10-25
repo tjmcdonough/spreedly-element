3a41c901c1269654c07f967571a9e2f3bc45f8a6
Tom McDonough <tmcdonough@airslip.com>
2022-10-25 10:34:28 +0400

    Added axios.defaults.withCredentials back in

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,7 +1,7 @@
 <template>
     <form id="payment-form" method="POST" novalidate="true" @submit="submitPaymentForm">
         <input id="payment_method_token" type="hidden" name="payment_method_token" />
-
+  
         <input
             id="full_name"
             type="text"
@@ -10,14 +10,14 @@
             style="min-width: 300px"
             placeholder="Name on the card"
         />
-
+  
         <div id="spreedly-number" class="spreedly-input" placeholder="4242 4242 4242 4242"></div>
-
+  
         <div style="display: flex">
             <input id="month" type="text" class="spreedly-input" name="month" maxlength="2" placeholder="MM" />
             <input id="year" type="text" class="spreedly-input" name="year" maxlength="4" placeholder="YYYY" /><br />
         </div>
-
+  
         <div
             id="spreedly-cvv"
             class="spreedly-input spreedly-bottom-input"
@@ -25,33 +25,33 @@
             placeholder="CVV"
         ></div>
         <br />
-
+  
         <input id="submit-button" type="submit" value="Continue" class="spreedly-pay-now" disabled />
     </form>
-</template>
-
-<script>
-import axios from 'axios';
-
-const serverUrl = 'https://dev.acmedao.com';
-const accessToken = window.vm.config.globalProperties.$cookie.getCookie('session');
-const headers = {
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  const serverUrl = 'https://dev.acmedao.com';
+  const accessToken = window.vm.config.globalProperties.$cookie.getCookie('session');
+  const headers = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
     Authorization: 'Bearer ' + accessToken,
     withCredentials: true,
-};
-
-async function loginToAcmeBackend() {
+  };
+  
+  async function loginToAcmeBackend() {
     try {
         await axios.post(`${serverUrl}/user/login`, {}, { headers });
         console.log('Successfully logged in with payment method');
     } catch (err) {
         console.log('Failed to log in ' + err);
     }
-}
-
-export default {
+  }
+  
+  export default {
     props: {
         content: { type: Object, required: true },
     },
@@ -65,11 +65,13 @@ export default {
     },
     async mounted() {
         console.log('mounted', { weWebId: this.weWebId });
-
+  
         let recaptchaScript = document.createElement('script');
         recaptchaScript.setAttribute('src', 'https://core.spreedly.com/iframe/iframe-v1.min.js');
         document.head.appendChild(recaptchaScript);
-
+  
+        axios.defaults.withCredentials = true;
+  
         function initialiseSpreedly() {
             console.log('trying spreedly init');
             if (window.Spreedly) {
@@ -77,14 +79,14 @@ export default {
                     numberEl: 'spreedly-number',
                     cvvEl: 'spreedly-cvv',
                 });
-
+  
                 // Start of on ready
                 window.Spreedly.on('ready', function () {
                     const submitButton = document.getElementById('submit-button');
                     submitButton.disabled = false;
                 });
                 // End of on ready
-
+  
                 // Start of on errors
                 window.Spreedly.on('errors', function (errors) {
                     for (var i = 0; i < errors.length; i++) {
@@ -96,7 +98,7 @@ export default {
                 // Start of on payment method
                 window.Spreedly.on('paymentMethod', async function (token, payment_method) {
                     console.log('on successful spreedly payment method');
-
+  
                     const addCard = {
                         token: token,
                         number: payment_method.number,
@@ -108,7 +110,7 @@ export default {
                         created_at: payment_method.created_at,
                         updated_at: payment_method.updated_at,
                     };
-
+  
                     try {
                         const response = await axios.post(`${serverUrl}/user/addCard`, addCard, { headers });
                         console.log('addCard success', response);
@@ -119,7 +121,7 @@ export default {
                     } finally {
                         this.loading = false;
                     }
-
+  
                     const createWyreCardTransaction = {
                         token: token,
                         number: payment_method.number,
@@ -137,7 +139,7 @@ export default {
                         tokenId: '77', // TODO: Get value
                         contractId: '0x0', // TODO: Get value
                     };
-
+  
                     try {
                         const response = await axios.post(
                             `${serverUrl}/user/createWyreCardTransaction`,
@@ -147,7 +149,7 @@ export default {
                             }
                         );
                         console.log('createWyreCardTransaction success', response);
-
+  
                         wwLib.wwVariable.updateValue(`${this.weWebId}-var_payment_complete`, true);
                     } catch (err) {
                         console.error(err);
@@ -161,7 +163,7 @@ export default {
                 setTimeout(() => initialiseSpreedly(), 100);
             }
         }
-
+  
         await loginToAcmeBackend();
         initialiseSpreedly();
     },
@@ -170,24 +172,24 @@ export default {
         submitPaymentForm(e) {
             e.preventDefault();
             console.log('spreedly submiting payment form');
-
+  
             var requiredFields = {};
             // Get required, non-sensitive, values from host page
             requiredFields['full_name'] = document.getElementById('full_name').value;
             requiredFields['month'] = document.getElementById('month').value;
             requiredFields['year'] = document.getElementById('year').value;
-
+  
             window.Spreedly.tokenizeCreditCard(requiredFields);
-
+  
             return false;
         },
     },
-};
-</script>
-
-<style scoped>
-.spreedly-input,
-.input-outline-none {
+  };
+  </script>
+  
+  <style scoped>
+  .spreedly-input,
+  .input-outline-none {
     width: 100%;
     height: 48px;
     border: 1px solid #ebebf4;
@@ -196,24 +198,24 @@ export default {
     color: #000;
     background-color: #fff;
     font-family: 'Montserrat', sans-serif;
-}
-
-.spreedly-input::placeholder,
-.input-outline-none::placeholder {
+  }
+  
+  .spreedly-input::placeholder,
+  .input-outline-none::placeholder {
     color: #cccccc;
-}
-
-.spreedly-top-input {
+  }
+  
+  .spreedly-top-input {
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
-}
-
-.spreedly-bottom-input {
+  }
+  
+  .spreedly-bottom-input {
     border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
-}
-
-.spreedly-pay-now {
+  }
+  
+  .spreedly-pay-now {
     background-color: rgb(78, 113, 228);
     border: 3px solid rgb(78, 113, 228);
     color: #fff;
@@ -224,10 +226,11 @@ export default {
     font-weight: 700;
     cursor: pointer;
     font-family: 'Montserrat', sans-serif;
-}
-
-.spreedly-pay-now:hover {
+  }
+  
+  .spreedly-pay-now:hover {
     background-color: rgba(78, 113, 228, 80);
     border: 3px solid rgb(123, 149, 234);
-}
-</style>
+  }
+  </style>
+  
