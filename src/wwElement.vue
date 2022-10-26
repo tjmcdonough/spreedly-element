ddfa5e4e3152f894de861bb3b822414fb16139f6
Ben Underwood <ben101@gmail.com>
2022-10-26 19:20:08 +0100

    update element config to expect a variable to be passed from weWeb

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,7 +1,7 @@
 <template>
     <form id="payment-form" method="POST" novalidate="true" @submit="submitPaymentForm">
         <input id="payment_method_token" type="hidden" name="payment_method_token" />
-  
+
         <input
             id="full_name"
             type="text"
@@ -10,14 +10,14 @@
             style="min-width: 300px"
             placeholder="Name on the card"
         />
-  
+
         <div id="spreedly-number" class="spreedly-input" placeholder="4242 4242 4242 4242"></div>
-  
+
         <div style="display: flex">
             <input id="month" type="text" class="spreedly-input" name="month" maxlength="2" placeholder="MM" />
             <input id="year" type="text" class="spreedly-input" name="year" maxlength="4" placeholder="YYYY" /><br />
         </div>
-  
+
         <div
             id="spreedly-cvv"
             class="spreedly-input spreedly-bottom-input"
@@ -25,44 +25,44 @@
             placeholder="CVV"
         ></div>
         <br />
-  
+
         <input id="submit-button" type="submit" value="Continue" class="spreedly-pay-now" disabled />
     </form>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  const serverUrl = 'https://dev.acmedao.com';
-  const accessToken = window.vm.config.globalProperties.$cookie.getCookie('session');
-  const headers = {
+</template>
+
+<script>
+import axios from 'axios';
+
+const serverUrl = 'https://dev.acmedao.com';
+const accessToken = window.vm.config.globalProperties.$cookie.getCookie('session');
+const headers = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
     Authorization: 'Bearer ' + accessToken,
     withCredentials: true,
-  };
-  
-  async function loginToAcmeBackend() {
+};
+
+async function loginToAcmeBackend() {
     try {
         await axios.post(`${serverUrl}/user/login`, {}, { headers });
         console.log('Successfully logged in with payment method');
     } catch (err) {
         console.log('Failed to log in ' + err);
     }
-  }
-  
-  export default {
+}
+
+export default {
     props: {
         content: { type: Object, required: true },
     },
-    
+
     data() {
-      let response = null;
-      let loading = true;
-      let errored = false;
-      /* wwEditor:start */
-      response = null;
-      /* wwEditor:end */
+        let response = null;
+        let loading = true;
+        let errored = false;
+        /* wwEditor:start */
+        response = null;
+        /* wwEditor:end */
         return {
             weWebId: 'bac36cd6-d0f5-4270-9ef9-ca3ddeb0ed76',
             response: null,
@@ -72,31 +72,30 @@
     },
     async mounted() {
         console.log('mounted', { weWebId: this.weWebId });
-  
+
         let recaptchaScript = document.createElement('script');
         recaptchaScript.setAttribute('src', 'https://core.spreedly.com/iframe/iframe-v1.min.js');
         document.head.appendChild(recaptchaScript);
-  
+
         axios.defaults.withCredentials = true;
-  
-  
-        function initialiseSpreedly() {
+
+        const initialiseSpreedly = () => {
             console.log('trying spreedly init');
             if (window.Spreedly) {
                 window.Spreedly.init('2JUJq2v4HcgLwMJCiZvzDJuTxd', {
                     numberEl: 'spreedly-number',
                     cvvEl: 'spreedly-cvv',
                 });
-  
+
                 // Start of on ready
                 window.Spreedly.on('ready', () => {
                     const submitButton = document.getElementById('submit-button');
                     submitButton.disabled = false;
                 });
                 // End of on ready
-  
+
                 // Start of on errors
-                window.Spreedly.on('errors', (errors) => {
+                window.Spreedly.on('errors', errors => {
                     for (var i = 0; i < errors.length; i++) {
                         const error = errors[i];
                         console.error(error);
@@ -106,7 +105,6 @@
                 // Start of on payment method
                 window.Spreedly.on('paymentMethod', (token, payment_method) => {
                     console.log('on successful spreedly payment method');
-  
                     const addCard = {
                         token: token,
                         number: payment_method.number,
@@ -118,19 +116,17 @@
                         created_at: payment_method.created_at,
                         updated_at: payment_method.updated_at,
                     };
-  
+
                     axios
-                      .post(`${serverUrl}/user/addCard`, addCard, { headers })
-                      .then((response) => {
-                        console.log(response);
-  
-                      })
-                      .catch((error) => {
-                        console.log(error)
-  
-                      })
-                      //.finally(() => this.loading = false)
-  
+                        .post(`${serverUrl}/user/addCard`, addCard, { headers })
+                        .then(response => {
+                            console.log(response);
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+                    //.finally(() => this.loading = false)
+
                     const createWyreCardTransaction = {
                         token: token,
                         number: payment_method.number,
@@ -148,29 +144,24 @@
                         tokenId: '77', // TODO: Get value
                         contractId: '0x0', // TODO: Get value
                     };
-  
-                     axios
-                      .post(`${serverUrl}/user/createWyreCardTransaction`, createWyreCardTransaction, { headers })
-                      .then((response) => {
-                        console.log(response);
-                       
-                        console.log('createWyreCardTransaction success', response);
-                        wwLib.wwVariable.updateValue(this.content.variableId, value);
-                        wwLib.wwVariable.updateValue(`${this.weWebId}-var_payment_complete`, true);
-                      })
-                      .catch((error) => {
-                        console.log(error)
-                      
-                      })
-                      //.finally(() => this.loading = false)
-  
+
+                    axios
+                        .post(`${serverUrl}/user/createWyreCardTransaction`, createWyreCardTransaction, { headers })
+                        .then(response => {
+                            console.log('createWyreCardTransaction success', response);
+                            this.updatePaymentComplete(true);
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        });
+                    //.finally(() => this.loading = false)
                 });
                 // End of on payment method
             } else {
                 setTimeout(() => initialiseSpreedly(), 100);
             }
-        }
-  
+        };
+
         await loginToAcmeBackend();
         initialiseSpreedly();
     },
@@ -179,24 +170,27 @@
         submitPaymentForm(e) {
             e.preventDefault();
             console.log('spreedly submiting payment form');
-  
+
             var requiredFields = {};
             // Get required, non-sensitive, values from host page
             requiredFields['full_name'] = document.getElementById('full_name').value;
             requiredFields['month'] = document.getElementById('month').value;
             requiredFields['year'] = document.getElementById('year').value;
-  
+
             window.Spreedly.tokenizeCreditCard(requiredFields);
-  
+
             return false;
         },
+        updatePaymentComplete(val) {
+            wwLib.wwVariable.updateValue(this.content.payment_complete, val);
+        },
     },
-  };
-  </script>
-  
-  <style scoped>
-  .spreedly-input,
-  .input-outline-none {
+};
+</script>
+
+<style scoped>
+.spreedly-input,
+.input-outline-none {
     width: 100%;
     height: 48px;
     border: 1px solid #ebebf4;
@@ -205,24 +199,24 @@
     color: #000;
     background-color: #fff;
     font-family: 'Montserrat', sans-serif;
-  }
-  
-  .spreedly-input::placeholder,
-  .input-outline-none::placeholder {
+}
+
+.spreedly-input::placeholder,
+.input-outline-none::placeholder {
     color: #cccccc;
-  }
-  
-  .spreedly-top-input {
+}
+
+.spreedly-top-input {
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
-  }
-  
-  .spreedly-bottom-input {
+}
+
+.spreedly-bottom-input {
     border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
-  }
-  
-  .spreedly-pay-now {
+}
+
+.spreedly-pay-now {
     background-color: rgb(78, 113, 228);
     border: 3px solid rgb(78, 113, 228);
     color: #fff;
@@ -233,11 +227,10 @@
     font-weight: 700;
     cursor: pointer;
     font-family: 'Montserrat', sans-serif;
-  }
-  
-  .spreedly-pay-now:hover {
+}
+
+.spreedly-pay-now:hover {
     background-color: rgba(78, 113, 228, 80);
     border: 3px solid rgb(123, 149, 234);
-  }
-  </style>
-  
+}
+</style>
--- ww-config.js
+++ ww-config.js
@@ -1,38 +1,40 @@
 export default {
-  editor: {
-    label: {
-      en: "My Element",
+    editor: {
+        label: {
+            en: 'Spreedly',
+        },
     },
-  },
-  properties: {
-    accessToken: {
-      label: {
-        en: "Access token",
-      },
-      type: "Text",
-      bindable: true,
+    properties: {
+        accessToken: {
+            label: {
+                en: 'Access token',
+            },
+            type: 'Text',
+            bindable: true,
+        },
+        auth0_jwt: {
+            label: {
+                en: 'Auth0 JWT token',
+            },
+            type: 'Text',
+            bindable: true,
+        },
+        payment_complete: {
+            label: {
+                en: 'Is payment complete?',
+            },
+            type: 'Variable',
+            options: {
+                types: ['Boolean'],
+            },
+        },
     },
-    auth0_jwt: {
-      label: {
-        en: "Auth0 JWT token",
-      },
-      type: "Text",
-      bindable: true
-    },
-    payment_complete: {
-      label: {
-        en: "Is payment complete?",
-      },
-      type: "Boolean",
-      bindable: true
-    }
-  }, 
-  variables: [
-    { 
-      name: 'var_payment_complete',
-      type: 'Boolean',
-      defaultValue: false,
-      bindable: true
-    }
-  ]
+    variables: [
+        {
+            name: 'var_payment_complete',
+            type: 'Boolean',
+            defaultValue: false,
+            bindable: true,
+        },
+    ],
 };
